b71dae0067c1d2b3bf3f6cd1ae555e5ba80c8c0b
tleavitt25526 <98976857+tleavitt25526@users.noreply.github.com>
2022-02-18 17:38:08 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,25 +3,68 @@ import java.util.*;
 
 public class Main
 {
+    // main method
 	public static void main(String[] args) {
+	    // set up a list full of the Currency class
 		ArrayList<Currency> classList = new ArrayList<>();
-		setup(classList);
+		setup(classList, "currencyFile.txt");
+		//printFile("currencyFile.txt");
+		
+		// loop until input ends asking user for conversions
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+		    String input = scan.nextLine();
+		    System.out.println(input);
+		}
 	}
 	
-	public static void setup(ArrayList<Currency> classList) {
-	   // eventually I want the program to read a file with really basic data.
-	   // data example in a file: "US Dollar,1.00"
-	   // each line would have that data for each currency.
-	   classList.add(new Currency("US Dollar", 1.00));
-	   classList.add(new Currency("Euro", 0.808407));
-	   classList.add(new Currency("British Pound", 0.735918));
-	   classList.add(new Currency("Indian Rupee", 75.094991));
-	   classList.add(new Currency("Australian Dollar", 1.388115));
-	   classList.add(new Currency("Canadian Dollar", 1.268464));
-	   classList.add(new Currency("Singapore Dollar", 1.342711));
-	   classList.add(new Currency("Swiss Franc", 0.921730	));
-	   classList.add(new Currency("Malaysian Ringgit", 4.185310));
-	   classList.add(new Currency("Japanese Yen", 115.426867));
-	   classList.add(new Currency("Chinese Yuan Renminbi", 6.337215));
+	// method to convert US dollars to a given currencyFile
+	public static double convert(String currencyName, ArrayList<Currency> classList, int amount) {
+	    // find the right class in the class list
+	    for (int i = 0; i < classList.size(); i++) {
+	        if (classList.get(i).getName() == currencyName) {
+	            // convert based on the given amount and exchange
+	            return classList.get(i).convert(amount);
+	        }
+	    }
+	    System.out.println("Invalid currencyName, classList, or amount");
+	    return amount;
 	}
+	
+	// method to read a given file and print each line in the console
+	public static void printFile(String fileName) {
+	    // try opening the file and reading it
+	    try {
+	        File textFile = new File(fileName);
+	        Scanner scan = new Scanner(textFile);
+	        // loop through the lines of file
+	        while (scan.hasNextLine()) {
+	            // print each line
+	            System.out.println(scan.nextLine());
+	        }
+	    } catch (FileNotFoundException e) {
+	        // if the file name was not found
+	        System.out.println("Invalid File.");
+	    }
+	}
+	
+	// method to read a given file and fill a given class list
+	public static void setup(ArrayList<Currency> classList, String fileName) {
+	    // try opening file and reading it
+	   try {
+    	   File textFile = new File(fileName);
+    	   Scanner scan = new Scanner(textFile);
+    	   // loop through lines of file
+    	   while (scan.hasNextLine()) {
+    	       // seperate lines into name and exhange and add them as a class into the class list
+    	       String line = scan.nextLine();
+    	       String[] data = line.split(",");
+    	       classList.add(new Currency(data[0], Double.parseDouble(data[1]) ));
+    	   }
+	   } catch (FileNotFoundException e) {
+	       // if the file name was not found
+	       System.out.println("Invalid File.");
+	   }
+	}
+	
 }
